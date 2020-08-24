34f9a97a2017b9d68e9314c8b20c896bf0eb7e88
miiga <miiga0509@gmail.com>
2020-08-23 23:45:19 -0700

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -5,14 +5,23 @@ var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 // Шоо нь аль талаараан буусныг хадгалах хувсагч
-var dice = Math.floor(Math.random() * 6) + 1;
+
 // window.document.querySelector("#score-0").textContent = dice;
 // console.log("Шоо " + dice);
 // document.querySelector("#score-1").innerHTML = "<em>Yess</em>";
 // console.log("Шоо " + dice);
 // Програм эхэлхэд бэлдэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
